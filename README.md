244254be793df421e03800968a2c11409b40a068
Gitlab auto-deploy <213991+leeppolis@users.noreply.github.com>
2020-12-11 17:28:23 +0000

    Ready to deploy

---
+++ README.md
@@ -0,0 +1,10 @@
+# ASINISTRA!
+
+Questo repository contiene il codice sorgente del sito pubblicato all'indirizzo https://asinistra.visualize.news
+
+Per segnalare problemi o errori, potete usare l'issue tracker di github: https://github.com/visualizenews/asinistra/issues
+
+# Licenza
+
+Il progetto e tutti i suoi file sono rilasciati con licenza MIT.
+
---
+++ precache-manifest.0ef0fadb89baad418a18481edf0537cd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d7ac1834b6a65f9896a6c32e763ad63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dae681754f927b8a999f",
+    "url": "/static/css/main.8f5897e8.chunk.css"
+  },
+  {
+    "revision": "6bb937b778fdcf6c893d",
+    "url": "/static/js/2.10dc5320.chunk.js"
+  },
+  {
+    "revision": "dae681754f927b8a999f",
+    "url": "/static/js/main.d5dc4fc6.chunk.js"
+  },
+  {
+    "revision": "d347a0c2c2e9660fa056",
+    "url": "/static/js/runtime~main.7fde81ca.js"
+  },
+  {
+    "revision": "8bedc00b0f7d8a39c00dfc8ab0733c3a",
+    "url": "/static/media/Connectors.8bedc00b.csv"
+  },
+  {
+    "revision": "37b35d36791effb831c97f49abe1e1f2",
+    "url": "/static/media/Lines.37b35d36.csv"
+  },
+  {
+    "revision": "d5b0fc69667561d2d5cd13923e36a95f",
+    "url": "/static/media/Logos.d5b0fc69.csv"
+  },
+  {
+    "revision": "f0726e4000a0be69fe8dff49492ae279",
+    "url": "/static/media/intro.f0726e40.png"
+  }
+]);
